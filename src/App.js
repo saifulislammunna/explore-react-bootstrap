297f03a131ee2e381ff91df6fe0acbf727864b38
saifulislammunna <saifulmunna28@gmail.com>
2021-09-27 23:10:41 +0600

    button-added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+ 
+ import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { ButtonGroup, Button } from 'react-bootstrap';
+import './App.css';
+ 
 function App() {
+  
+
   return (
     <div className="App">
-      
-    </div>
+      <Button variant="primary">Hello me</Button>
+     
+     
+   </div>
   );
 }
 
