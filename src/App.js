97b0730f19c8922b8adb3a3459ccdf3c18e30610
saifulislammunna <saifulmunna28@gmail.com>
2021-09-28 10:17:53 +0600

    news api added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
  
  import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect, useState } from 'react';
 
-import { ButtonGroup, Button } from 'react-bootstrap';
+import { ButtonGroup, Button, Spinner } from 'react-bootstrap';
 import './App.css';
  
 function App() {
-  
+  const [news, setNews] = useState([])
+  useEffect(() => {
+    fetch('https://newsapi.org/v2/everything?q=tesla&from=2021-08-28&sortBy=publishedAt&apiKey=4444210250cf4ddc814444c4d934ed53')
+    .then(res => res.json())
+    .then(data => setNews(data));
+  }, [])
 
   return (
     <div className="App">
       <Button variant="primary">Hello me</Button>
+      <Spinner animation="border" role="status">
+  <span className="visually-hidden">Loading...</span>
+</Spinner>
      
      
    </div>
