b2ddec6a0862a94ee145c6b9dc0532d06e7130a1
saifulislammunna <saifulmunna28@gmail.com>
2021-09-28 19:29:59 +0600

    spinner-added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
  
  import 'bootstrap/dist/css/bootstrap.min.css';
 import { useEffect, useState } from 'react';
-
-import { ButtonGroup, Button, Spinner } from 'react-bootstrap';
-import './App.css';
+import {   Row, Spinner } from 'react-bootstrap';
  
+import './App.css';
+import News from './components/News/News';
+
+// const c = [];
+// const number = c.length === 0 ? 5 : 10;
+
 function App() {
   const [news, setNews] = useState([])
   useEffect(() => {
     fetch('https://newsapi.org/v2/everything?q=tesla&from=2021-08-28&sortBy=publishedAt&apiKey=4444210250cf4ddc814444c4d934ed53')
     .then(res => res.json())
-    .then(data => setNews(data));
+    .then(data => setNews(data.articles));
   }, [])
 
   return (
     <div className="App">
-      <Button variant="primary">Hello me</Button>
-      <Spinner animation="border" role="status">
-  <span className="visually-hidden">Loading...</span>
-</Spinner>
+       
+       {
+        news.length === 0 ?
+        <Spinner animation="border" />  :
+         <Row xs={1} md={3} className="g-4">
+         {
+            news.map(nw => <News news={nw}></News>)
+         }
+    
+   
+       </Row>
+       }
      
      
    </div>
